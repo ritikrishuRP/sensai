e22e5bd2222bd86592803734c4ae718d28706ed7
rishu <rrishu212@gmail.com>
2025-05-05 20:59:32 +0530

    Home page and database

--- app/layout.js
+++ app/layout.js
@@ -3,6 +3,7 @@ import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
 import Header from "@/components/header";
 import { ClerkProvider } from "@clerk/nextjs";
+import { dark } from "@clerk/themes"
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +17,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <ClerkProvider appearance={{
-      baseTheme: "dark"
+      baseTheme: dark,
     }}>
       <html lang="en" suppressHydrationWarning>
       <body
@@ -31,7 +32,7 @@ export default function RootLayout({ children }) {
            {/* header */}
            <Header />
            {/* main */}
-           <main className="min-h-screen">{children}</main>
+           <main className="min-h-screen pt-16">{children}</main>
            {/* footer */}
            <footer className="bg-muted/50 py-12">
             <div className="container mx-auto px-4 text-center text-gray-200">
